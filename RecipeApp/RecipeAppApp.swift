ad43bd3fd1e11f012cdf64ff316e8455dd21fa31
Bholanath <bholanathbarik9748@gmail.com>
2024-08-05 22:54:32 +0530

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Bholanath Barik on 05/08/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
