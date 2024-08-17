ed67a96e2855a75bcd2ac0c6091733ee48c889f4
Bholanath <bholanathbarik9748@gmail.com>
2024-08-18 00:23:12 +0530

    Feat: Add Youtube Deeplink and UI improvement

---
+++ RecipeApp/Shared/YouTubeBtn/YouTubeBtn.swift
@@ -0,0 +1,18 @@
+//
+//  YouTubeBtn.swift
+//  RecipeApp
+//
+//  Created by Bholanath Barik on 17/08/24.
+//
+
+import SwiftUI
+
+struct YouTubeBtn: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    YouTubeBtn()
+}
